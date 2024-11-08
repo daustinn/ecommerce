98092234497587d7ad49de4eb915caeeea69d15e
Daustinn <daustinn.music@gmail.com>
2024-11-08 01:02:34 -0500

    Refactor next.config.ts to update image remotePatterns Update package.json to include typegen script Add sanity.types.ts for Sanity TypeGen

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['img.clerk.com']
+    remotePatterns: [
+      {
+        hostname: 'img.clerk.com',
+        protocol: 'https'
+      }
+    ]
   }
 }
 
