00bd5b135a74f3123b0534dc1a0d736704e2bb09
Daustinn <daustinn.music@gmail.com>
2024-11-09 18:07:04 -0500

    Refactor store page to remove unused code and improve performance

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,17 +1,11 @@
 import Hero from '@/components/hero'
-// import HeroTenProducts from '@/components/hero/products'
 import { getFourCollections } from '@/sanity/lib/collections/getFourCollections'
-// import { getTenProducts } from '@/sanity/lib/products/getTenProducts'
-import React from 'react'
 
 export default async function HomePage() {
   const collections = await getFourCollections()
-  // const products = await getTenProducts()
-
   return (
     <div className="w-full">
       <Hero collections={collections} />
-      {/* <HeroTenProducts products={products} /> */}
     </div>
   )
 }
