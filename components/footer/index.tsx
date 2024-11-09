6695a196e410a5eec6ff5ea7eccfe0c3a7d147fa
Daustinn <daustinn.music@gmail.com>
2024-11-08 21:28:04 -0500

    Add footer component

---
+++ components/footer/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className="p-5 text-sm text-center">
+      This is a demo Ecommerce Created by{' '}
+      <a
+        href="https://daustinn.com"
+        target="_blank"
+        className="hover:underline"
+      >
+        &copy;Daustinn
+      </a>
+    </div>
+  )
+}
