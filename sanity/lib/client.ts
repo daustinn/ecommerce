4ed2476b38158a385716c94e0793a3113acd63af
Daustinn <daustinn.music@gmail.com>
2024-11-11 18:07:03 -0500

    Refactor studioUrl in sanity/lib/client.ts

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,7 +6,7 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation,
+  useCdn: true,
   stega: {
     studioUrl: process.env.NEXT_PUBLIC_BASE_URL + '/studio'
   }
