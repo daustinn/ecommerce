91bce17f7696831965d1797f5978aad2e7d60918
Daustinn <daustinn.music@gmail.com>
2024-11-10 19:12:25 -0500

    Refactor cart store to display shopping cart item count in header

--- components/header/right.tsx
+++ components/header/right.tsx
@@ -1,18 +1,28 @@
+'use client'
+
 import Link from 'next/link'
 import React from 'react'
 import { GiShoppingCart } from 'react-icons/gi'
 import { IoIosSearch } from 'react-icons/io'
 import User from './user'
+import useCartStore from '@/stores/cart.store'
 
 export default function RightHeader() {
+  const items = useCartStore((state) => state.items)
+  const totalItems = items.reduce((acc, item) => acc + item.quantity, 0)
   return (
     <nav className="flex flex-grow items-center gap-8 basis-0 justify-end">
       <Link
-        className="hidden lg:flex"
+        className="hidden lg:flex relative"
         href="/cart"
         title="Mi carrito de compra"
       >
         <GiShoppingCart size={25} />
+        {totalItems > 0 && (
+          <div className="absolute w-5 h-5 grid place-content-center -top-2 -right-2 rounded-full bg-blue-700 text-white text-xs">
+            {totalItems}
+          </div>
+        )}
       </Link>
       <Link
         className="hidden lg:flex"
