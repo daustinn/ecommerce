e6408f009eee028f1654e765c6d992c1b9733996
Daustinn <daustinn.music@gmail.com>
2024-11-10 19:12:43 -0500

    Add Stripe integration for payment processing

---
+++ lib/stripe.ts
@@ -0,0 +1,11 @@
+import Stripe from 'stripe'
+
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error('Please define STRIPE_SECRET_KEY as an environment variable')
+}
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2024-10-28.acacia'
+})
+
+export default stripe
